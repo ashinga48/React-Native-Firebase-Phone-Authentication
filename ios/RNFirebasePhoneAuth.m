7135990eff1c3f6fe70cbdb675d8b09f9850a33f
ashinga48 <ashinga48@gmail.com>
2018-05-08 12:39:03 +1000

    Updated SendOTP function in package

--- ios/RNFirebasePhoneAuth.m
+++ ios/RNFirebasePhoneAuth.m
@@ -1,7 +1,9 @@
 
 #import "RNFirebasePhoneAuth.h"
-//#import <Firebase/Firebase.h>
-
+#import <Firebase/Firebase.h>
+#import <FirebaseAuth/FirebaseAuth.h>
+//@import Firebase;
+//@import FirebaseAuth;
 
 @implementation RNFirebasePhoneAuth
 
@@ -23,39 +25,81 @@ RCT_REMAP_METHOD(testing,
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-//    float volume = [AVAudioSession sharedInstance].outputVolume;
+    //    float volume = [AVAudioSession sharedInstance].outputVolume;
     NSString* volumeString = @"getttingg ";
     NSLog(@"DDDDampleeeee");
-
+    
     if (volumeString) {
         resolve(volumeString);
     } else {
         reject(@"get_error", @"Error getting system volume", nil);
     }
-
+    
 }
 
 RCT_EXPORT_METHOD(initFB:(NSString *)fileName
                   callback:(RCTResponseSenderBlock)successCallback
                   errorCallback:(RCTResponseSenderBlock)failureCallback) {
     NSString* volumeString = @"getttingg ";
-
+    
     if (volumeString) {
         NSDictionary *resultsDict = @{
-              @"success" : @" SOME DATA RETURN ",
-              @"error"  : @NO
-          };
+                                      @"success" : @" SOME DATA RETURN ",
+                                      @"error"  : @NO
+                                      };
         // Execute the JavaScript failure callback handler
         successCallback(@[resultsDict]);
     } else {
         NSDictionary *resultsDict = @{
-          @"success" : @" SOME ERROR ",
-          @"error"  : @YES
-          };
+                                      @"success" : @" SOME ERROR ",
+                                      @"error"  : @YES
+                                      };
         // Execute the JavaScript failure callback handler
         failureCallback(@[resultsDict]);
     }
+    
+}
 
+RCT_EXPORT_METHOD(sendOTP:(NSString *)phoneNumber
+                  callback:(RCTResponseSenderBlock)successCallback
+                  errorCallback:(RCTResponseSenderBlock)failureCallback) {
+    
+    if (phoneNumber) {
+        
+        [[FIRPhoneAuthProvider provider]
+         verifyPhoneNumber:phoneNumber
+         completion:^(NSString * _Nullable verificationID, NSError * _Nullable error) {
+             
+             NSLog(@"data error %@", [error localizedDescription]);
+             
+             if(!error){
+                 NSDictionary *resultsDict = @{
+                                               @"verificationId" : verificationID,
+                                               @"error"  : @NO
+                                               };
+                 successCallback(@[resultsDict]);
+                 
+             }else{
+                 NSDictionary *resultsDict = @{
+                                               @"message" : verificationID,
+                                               @"error"  : [error localizedDescription]
+                                               };
+                 failureCallback(@[resultsDict]);
+             }
+             
+         }];
+        
+    } else {
+        NSDictionary *resultsDict = @{
+                                      @"message" : @"No Phone Number",
+                                      @"error"  : @YES
+                                      };
+        // Execute the JavaScript failure callback handler
+        failureCallback(@[resultsDict]);
+    }
+    
 }
 
 @end
+
+
